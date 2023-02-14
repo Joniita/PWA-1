1501f9dcb40f50a2d85dc9857b48261c19020d24
Joniita <joniita.d@gmail.com>
2023-02-14 02:06:27 -0300

    primera configuracion del service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+const STATIC_CACHE = "static";
+
+const APP_SHELL = [
+    '/',
+    'css/*',
+    'js/main.js',
+    'img/*',
+];
+
+self.addEventListener("install", e => {
+    const cacheStatic = caches
+        .open(STATIC_CACHE)
+        .then((cache) => cache.addAll(APP_SHELL));
+
+    e.waitUntil(cacheStatic);
+});
