7dc933cdad6a826c7a148c26078606dc07edb314
Joniita <joniita.d@gmail.com>
2023-02-15 00:40:28 -0300

    agrego modo offline

--- sw.js
+++ sw.js
@@ -15,3 +15,10 @@ self.addEventListener("install", e => {
     e.waitUntil(cacheStatic);
 });
 
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request)
+            .then((res) => res || fetch(e.request))
+            .catch(console.log)
+        );
+});
